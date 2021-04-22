3ce8115737d2bd7d3c4475560000d0e4b49aa9ec
Derrick Johnson <10237736+dj51234@users.noreply.github.com>
2021-04-22 01:00:24 -0400

    footer component rendered, mobile styles need to be done

--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { images } from "../../constants";
 import { Newsletter, FooterOverlay } from "../../components";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 import "./Footer.css";
@@ -7,6 +8,41 @@ const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019</p>
+        <p className="p__opensans">212-344-1230</p>
+        <p className="p__opensans">212-555-1230</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="logo" />
+        <p className="p__opensans" style={{ textTransform: "none" }}>
+          "The best way to find yourself is to lose yourself in the service of
+          others"
+        </p>
+        <img src={images.spoon} className="spoon__img" alt="spoon" />
+        <div className="app__footer-links_icons">
+          <FiFacebook fontSize={24} />
+          <FiTwitter fontSize={24} />
+          <FiInstagram fontSize={24} />
+        </div>
+      </div>
+      <div className="app__footer-links_hours">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <div style={{ marginBlock: "1rem" }}>
+          <p className="p__opensans">Monday-Friday</p>
+          <p className="p__opensans">8:00 am - 12:00 am</p>
+        </div>
+        <div>
+          <p className="p__opensans">Saturday-Sunday</p>
+          <p className="p__opensans">7:00 am - 11:00 pm</p>
+        </div>
+      </div>
+    </div>
+    <div className="app__footer_copyright">
+      <p className="p__opensans">Copyright Gericht. 2021</p>
+    </div>
   </div>
 );
 
